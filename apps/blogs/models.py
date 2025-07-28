9bc61b91f38fbd3988a2a376f3137f8b29a65b10
vashisth2526 <himanshu@elitemindz.co>
2025-07-28 22:42:30 +0530

    blogs model

---
+++ apps/blogs/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column, String, Integer, func, ForeignKey, DateTime, Boolean, Text, UniqueConstraint
+from apps.accounts.models import AuthUsers
+from config.database import Base
+
+class BaseModel(Base):
+    __abstract__ = True
+    is_active          = Column(Boolean, default=True, nullable=False)
+    created_at         = Column(DateTime, nullable=False, default=func.now())
+    updated_at         = Column(DateTime, nullable=True, onupdate=func.now()) 
+
+class Blogs(BaseModel):
+    __tablename__ = 'blogs_master'
+    id                 = Column(Integer, primary_key=True, index=True)
+    author_id          = Column(ForeignKey('auth_users.id', ondelete='RESTRICT'), nullable=False)
+    title              = Column(String(100), nullable=False)
+    description        = Column(Text, nullable=False)
+    attachment         = Column(String(255), nullable=True)
+
+class BlogsLikes(BaseModel):
+    __tablename__ = 'blogs_likes'
+    id                 = Column(Integer, primary_key=True, index=True)
+    blog_id            = Column(ForeignKey('blogs_master.id', ondelete='CASCADE'), nullable=False)
+    user_id            = Column(ForeignKey('auth_users.id', ondelete='SET NULL'), nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('blog_id', 'user_id', name='uix_blog_like'),
+    )
+
+class BlogsComments(BaseModel):
+    __tablename__ = 'blogs_comments'
+    id                 = Column(Integer, primary_key=True, index=True)
+    blog_id            = Column(ForeignKey('blogs_master.id', ondelete='RESTRICT'), nullable=False)
+    sender_id          = Column(ForeignKey('auth_users.id', ondelete='RESTRICT'), nullable=False)
+    comment            = Column(String(255), nullable=False)
+    sub_sender_id      = Column(ForeignKey('blogs_comments.id'), nullable=True)
